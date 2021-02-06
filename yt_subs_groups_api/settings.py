a2c777791f7f676ebbe0e246c4a992150e44f6b0
Nymfeparakit <maxlex047@gmail.com>
2021-02-06 12:44:22 +0300

    Change database backend to postgres

--- yt_subs_groups_api/settings.py
+++ yt_subs_groups_api/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'yt_subs_groups_api.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'yt_subs_groups_db',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
