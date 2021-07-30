2b54adee9a30255296cb83acde6794e6e971397c
Nymfeparakit <maxlex047@gmail.com>
2021-07-30 12:55:09 +0300

    Add missing url

--- yt_subs_groups_api/urls.py
+++ yt_subs_groups_api/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('feeds.urls')),
+    path('', include('users.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
